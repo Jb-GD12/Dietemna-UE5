f7ce02ba8254509190b4f519fb1e8f91250c5ef2
Tanguy LG <freiky83@outlook.fr>
2022-04-21 19:29:01 +0200

    Correction changement de version
    Ajout du cpp manquant
    Remake en gros du taf de vendredi 15/04

---
+++ Source/Dietemna/PlayerComponent.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerComponent.h"
+
+// Sets default values for this component's properties
+UPlayerComponent::UPlayerComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	m_taskValue = 1;
+	// ...
+}
+
+
+// Called when the game starts
+void UPlayerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+int UPlayerComponent::GetDayValue() {
+	return m_dayValue;
+}
+
+int UPlayerComponent::GetTaskValue() {
+	return m_taskValue;
+}
+
+void UPlayerComponent::SetTaskValue(int p_taskValue) {
+	m_taskValue = p_taskValue;
+}
+
+void UPlayerComponent::IncreaseTaskValue() {
+	m_taskValue++;
+}
+
+// Called every frame
+void UPlayerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/Dietemna/PlayerComponent.h
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PlayerComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class DIETEMNA_API UPlayerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UPlayerComponent();
+
+	UPROPERTY(EditAnywhere, Category = "LD")
+		int m_dayValue;
+
+	/// <summary>
+	/// Get the day value
+	/// </summary>
+	UFUNCTION(BlueprintCallable)
+		int GetDayValue();
+
+	/// <summary>
+	/// Get the task value
+	/// </summary>
+	UFUNCTION(BlueprintCallable)
+		int GetTaskValue();
+
+	/// <summary>
+	/// Set the task value
+	/// </summary>
+	UFUNCTION(BlueprintCallable)
+		void SetTaskValue(int p_taskValue);
+
+	/// <summary>
+	/// Increase the task value by +1
+	/// </summary>
+	UFUNCTION(BlueprintCallable)
+		void IncreaseTaskValue();
+
+private:
+	// VARIABLES
+	UPROPERTY()
+		int m_taskValue;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
